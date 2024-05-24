b9d5078f80a0c081eb6dc7b766d1558410b26c05
late autumn <161944760+12wangms@users.noreply.github.com>
2024-05-24 08:53:49 +0800

    Add files via upload

---
+++ 传入n个字典返回max min.py
@@ -0,0 +1,12 @@
+def func(*args):
+    the_max=args[0]
+    the_min=args[0]
+    for i in args:
+        if i>the_max:
+            the_max=i
+        else:i<the_min;
+        the_min=i
+        return{'max':the_max,'min':the_min}
+        es=func(1,20,3,40,5)
+        print(res)
+
